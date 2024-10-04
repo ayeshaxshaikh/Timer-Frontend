357880736d3febf904c719fecae43610b0f942e3
ayeshaxshaikh <ayesha.shaikh0318@gmail.com>
2024-10-04 21:31:16 +0530

    Added: Frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,43 @@
 
+// import './App.css';
+// import Timer from './components/Timer';
+// import { Routes, Route } from 'react-router-dom';
+// import TimerControl from './components/TimerControl';
+
+// function App() {
+//   return (
+//     <>
+//       <Routes>
+//         <Route path='/' element={<Timer />} />
+//         <Route path='/button' element={<TimerControl />} />
+//       </Routes>  
+//     </>
+//   );
+// }
+
+// export default App;
+
+
 import './App.css';
 import Timer from './components/Timer';
-import { Routes, Route } from 'react-router-dom';
 import TimerControl from './components/TimerControl';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Test() {
+  return <h1>This is a Test Page</h1>;
+}
 
 function App() {
   return (
-    <>
+    <Router>
       <Routes>
-        <Route path='/' element={<Timer />} />
-        <Route path='/button' element={<TimerControl />} />
-      </Routes>  
-    </>
+        <Route path="/" element={<Timer />} />
+        <Route path="/button" element={<TimerControl />} />
+        <Route path="/test" element={<Test />} /> {/* New test route */}
+      </Routes>
+    </Router>
   );
 }
 
-export default App;
-
+ export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    {/* <Router> */}
       <App />
-    </Router>
+    {/* </Router> */}
   </StrictMode>,
 )
